204dd0819cbb5e7164f1f7250e069f4c2dd7ac20
Mutou <839608583@qq.com>
2017-09-14 16:37:33 +0800

    Update app.main.js

--- 2.Node.js-basics/app.main.js
+++ 2.Node.js-basics/app.main.js
@@ -1,6 +1,6 @@
 var testModule = require('./testModule.js');
-console.log(testModule.testVar);
+// console.log(testModule.testVar);
 
-if(module === require.main) {
-    console.log('This is the main module of application.');
-}
+// if(module === require.main) {
+//     console.log('This is the main module of application.');
+// }
