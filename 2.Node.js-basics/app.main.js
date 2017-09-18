d4fee3621fd96c1313b26216dca1cb08f465c743
Mutou <839608583@qq.com>
2017-09-18 16:31:57 +0800

    Delete app.main.js

--- 2.Node.js-basics/app.main.js
+++
@@ -1,6 +0,0 @@
-var testModule = require('./testModule.js');
-// console.log(testModule.testVar);
-
-// if(module === require.main) {
-//     console.log('This is the main module of application.');
-// }
