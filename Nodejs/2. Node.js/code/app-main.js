7cc51445fc154a13c8198f777d5af606370eb6d7
Mutou <839608583@qq.com>
2017-09-18 16:41:42 +0800

    Create app-main.js

---
+++ Nodejs/2. Node.js/code/app-main.js
@@ -0,0 +1,6 @@
+var testModule = require('./testModule.js');
+// console.log(testModule.testVar);
+
+// if(module === require.main) {
+//     console.log('This is the main module of application.');
+// }
